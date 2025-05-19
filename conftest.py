9ada2f24414e78071f530abf5add7099fa0640c4
KrasDev <you@example.com>
2025-05-19 19:12:14 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -6,7 +6,8 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope='class')
 def browser():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(args=['--start-maximized'], headless=False)
+        browser = playwright.chromium.launch(args=['--no-sandbox', '--disable-dev-shm-usage'],
+                                             headless=True)
         context = browser.new_context(ignore_https_errors=True)
         page = context.new_page()
         yield page
